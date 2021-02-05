097070aaac31b11b59e8066bbc5853f5de9f9e58
Richard Arthurs <2465714A@student.gla.ac.uk>
2021-02-05 15:03:53 +0000

    First templates added!

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context=context_dict)
+
     return HttpResponse("Rango says hey there partner!")
 
-
+def about(request):
+    return HttpResponse("Rango says here is the about page")
